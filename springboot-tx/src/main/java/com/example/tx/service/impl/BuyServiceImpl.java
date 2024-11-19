b930043b25362da2dd6371f0cc181c123a5dd61d
YangYangCode <yangyangchencode@gmail.com>
2024-11-19 11:32:06 +0800

    buyService

---
+++ springboot-tx/src/main/java/com/example/tx/service/impl/BuyServiceImpl.java
@@ -0,0 +1,34 @@
+package com.example.tx.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.tx.service.BookService;
+import com.example.tx.service.BuyService;
+
+@Service	// 交易服務
+public class BuyServiceImpl implements BuyService {
+
+	@Autowired
+	private BookService bookService;
+
+	@Override
+	public void buyOneBook(String username, Integer bookId) {
+		System.out.println(username+"購買書籍");
+		
+		// 1. 查詢書本價格
+		Integer bookPrice = bookService.getBookPrice(bookId);
+		System.out.println("bookId price: "+bookPrice);
+		
+		// 2. 減去庫存 (1本)
+		bookService.reduceBookAmount(bookId, 1);
+		
+		// 3. 修改餘額
+		bookService.reduceWalletBalance(username, bookPrice);
+		
+		// 4. 其他處理
+		System.out.println(username+"結帳完成...");
+		
+	}
+
+}
