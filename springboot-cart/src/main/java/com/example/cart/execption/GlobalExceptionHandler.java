700db1c61809895ccfa689ba6640145ae7cf50b0
YangYangCode <yangyangchencode@gmail.com>
2024-11-27 11:02:49 +0800

    各別顯示exception -> GlobalException (加定義格式)

---
+++ springboot-cart/src/main/java/com/example/cart/execption/GlobalExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.example.cart.execption;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.example.cart.response.ApiResponse;
+
+@ControllerAdvice	// 全域例外
+public class GlobalExceptionHandler {
+
+	@ExceptionHandler(UnauthorizedException.class)	// 捕獲指定例外
+	public ResponseEntity<ApiResponse<String>> handelUnauthorizedException(UnauthorizedException ex) {
+		ApiResponse<String> response = ApiResponse.error(HttpStatus.FORBIDDEN.value(), "未登入或登入失敗");
+		return ResponseEntity.status(HttpStatus.FORBIDDEN).body(response);		// 沒有session，禁止訪問
+	}
+}
