696d38fb6a791e87c7bb5067a6cbf21594242bde
YangYangCode <yangyangchencode@gmail.com>
2024-12-03 10:22:31 +0800

    業務邏輯與 Before、After 分離

--- springboot-proxy-aop/src/main/java/com/example/proxy/service/impl/PaymentServiceImpl.java
+++ springboot-proxy-aop/src/main/java/com/example/proxy/service/impl/PaymentServiceImpl.java
@@ -1,5 +1,8 @@
 package com.example.proxy.service.impl;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.springframework.stereotype.Service;
 
 import com.example.proxy.service.PaymentService;
@@ -8,15 +11,17 @@ import com.example.proxy.service.PaymentService;
 public class PaymentServiceImpl implements PaymentService {
 
 	@Override
-	public void pay(int amount) {
+	public void pay(int amount) {	// 業務方法 - 付款
+		// 業務邏輯
 		System.out.println("支付 "+ amount +" 元");
-
 	}
+	
 
 	@Override
-	public void refund(int amount) {
+	public void refund(int amount) {	// 業務方法 - 退款
+		// 業務邏輯
 		System.out.println("退款 "+ amount +" 元");
-
 	}
 
+	
 }
---
+++ springboot-proxy-aop/src/main/java/com/example/proxy/service/impl/PaymentServiceProxy.java
@@ -0,0 +1,52 @@
+package com.example.proxy.service.impl;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.proxy.service.PaymentService;
+
+public class PaymentServiceProxy implements PaymentService {
+
+	private PaymentService paymentService;
+
+	public PaymentServiceProxy(PaymentService paymentService) {
+		this.paymentService = paymentService;
+	}
+	
+	
+	@Override
+	public void pay(int amount) {
+		// Before 執行業務邏輯前檢查 - 檢查金額
+		if(amount <= 0) {
+			System.out.println("支付失敗: 金額必須大於 0 元");
+			return;
+		}
+		
+		// 代理執行實際支付業務
+		paymentService.pay(amount);
+		
+		// After 執行業務邏輯後: 紀錄log
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		System.out.println("支付 "+ amount +" 元" + sdf.format(new Date()));
+	}
+
+	
+	@Override
+	public void refund(int amount) {
+		// Before 執行業務邏輯前檢查 - 檢查金額
+		if(amount <= 0) {
+			System.out.println("退款失敗: 金額必須大於 0 元");
+			return;
+		}
+		
+		// 代理執行實際退款業務
+		paymentService.pay(amount);
+				
+		// After 執行業務邏輯後: 紀錄log
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		System.out.println("支付 "+ amount +" 元" + sdf.format(new Date()));
+	}
+
+}
