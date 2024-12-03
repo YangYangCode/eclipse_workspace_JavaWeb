cb3238afa0550c0a453542dac1d78d3a0ca37f2d
YangYangCode <yangyangchencode@gmail.com>
2024-12-03 10:04:05 +0800

    AOP_Service_test

---
+++ springboot-proxy-aop/src/main/java/com/example/proxy/service/impl/PaymentServiceImpl.java
@@ -0,0 +1,22 @@
+package com.example.proxy.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.example.proxy.service.PaymentService;
+
+@Service
+public class PaymentServiceImpl implements PaymentService {
+
+	@Override
+	public void pay(int amount) {
+		System.out.println("支付 "+ amount +" 元");
+
+	}
+
+	@Override
+	public void refund(int amount) {
+		System.out.println("退款 "+ amount +" 元");
+
+	}
+
+}
